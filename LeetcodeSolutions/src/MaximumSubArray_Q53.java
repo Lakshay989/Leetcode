bb73c2a4cb1820d26be2706cc4faa0db22dd3430
Lakshay989 <lakshaykucheriya@gmail.com>
2022-12-12 23:55:46 -0700

    Updating my repo

---
+++ LeetcodeSolutions/src/MaximumSubArray_Q53.java
@@ -0,0 +1,24 @@
+class MaximumSubArray_Q53 {
+
+    public static void main(String[] args) {
+        //MaximumSubArray_Q53 obj ;
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println();
+        System.out.println(maxSubArray(nums));
+
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+
+    }
