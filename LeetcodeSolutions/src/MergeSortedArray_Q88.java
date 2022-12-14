d2821255f5e2bd77fd2460d7d8cd3440db17d97f
Lakshay989 <lakshaykucheriya@gmail.com>
2022-12-14 00:21:14 -0700

    Updating my repo

---
+++ LeetcodeSolutions/src/MergeSortedArray_Q88.java
@@ -0,0 +1,42 @@
+public class MergeSortedArray_Q88
+{
+
+    public static void main(String[] args)
+    {
+        int[] nums1 = {1,2,3,0,0,0} ;
+        int m = 3 ;
+        int[] nums2 = {2,5,6} ;
+        int n = 3 ;
+        merge(nums1, m, nums2, n) ;
+        for (int i = 0 ; i< nums1.length ; i++)
+        {
+            System.out.println(nums1[i]);
+        }
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        // Three pointer approach
+
+        // Make a copy of the first m elements of nums1.
+        int[] nums1Copy = new int[m];
+        for (int i = 0; i < m; i++) {
+            nums1Copy[i] = nums1[i];
+        }
+
+        // Read pointers for nums1Copy and nums2 respectively.
+        int p1 = 0;
+        int p2 = 0;
+
+        // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+        for (int p = 0; p < m + n; p++) {
+            // We also need to ensure that p1 and p2 aren't over the boundaries
+            // of their respective arrays.
+            if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+                nums1[p] = nums1Copy[p1++];
+            } else {
+                nums1[p] = nums2[p2++];
+            }
+        }
+    }
+}
