93117e01b2b1059cc01dd36fe7d959c9bfe2c867
Lakshay989 <lakshaykucheriya@gmail.com>
2022-12-19 12:38:54 -0700

    Updating my repo

---
+++ LeetcodeSolutions/src/ValidAnagram_Q242.java
@@ -0,0 +1,28 @@
+public class ValidAnagram_Q242
+{
+    public static void main(String[] args)
+    {
+        String s = "anagram";
+        String t = "nagaram" ;
+        System.out.println(isAnagram(s,t));
+    }
+
+    public static boolean isAnagram(String s, String t)
+    {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
